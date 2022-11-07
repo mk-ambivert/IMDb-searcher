dbd13a3f4c7e1c26727ab71e2392f813ef089584
mk-ambivert <mv.koska@gmail.com>
2022-11-07 08:47:11 +0200

    Ui dependencies updated.

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"github.com/IMDb-searcher/internal/cli/adapter"
+	"github.com/IMDb-searcher/internal/logger"
 )
 
 type IUserInterface interface {
@@ -12,18 +13,20 @@ type UserInterface struct {
 	IUserInterface
 
 	formatter adapter.IFormatter
+	log       logger.ILogger
 }
 
 func (u *UserInterface) Run() {
 
 }
 
-func New() (IUserInterface, error) {
-	formatter, err := adapter.New()
+func New(logger logger.ILogger) (IUserInterface, error) {
+	formatter, err := adapter.New(logger)
 	if err != nil {
 		return nil, err
 	}
 	return &UserInterface{
 		formatter: formatter,
+		log:       logger,
 	}, nil
 }
